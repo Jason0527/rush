630e78e60084b3b84f5cdff504a27496c2685c85
Jason0527 <ningjinsen@163.com>
2019-04-23 17:46:49 +0800

    add testOther2

---
+++ src/test/java/com/jason/boot/testOthers/TestOther2.java
@@ -0,0 +1,10 @@
+package com.jason.boot.testOthers;
+
+import org.junit.Test;
+
+public class TestOther2 {
+	@Test
+	public void test(){
+		System.out.println("athis is testOther2 for git ");
+	}
+}
